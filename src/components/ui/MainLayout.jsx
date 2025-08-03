f8a68cfa7479fa6b5e6fc561d20dfb22bcdc7c1c
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-08-03 10:42:27 +0000

    chore: Comment out Analytics import in MainLayout component

--- src/components/ui/MainLayout.jsx
+++ src/components/ui/MainLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Analytics } from "@vercel/analytics/next"
+// import { Analytics } from "@vercel/analytics/next"
 import Navbar from '../layout/Navbar';
 import Footer from '../layout/Footer';
 
